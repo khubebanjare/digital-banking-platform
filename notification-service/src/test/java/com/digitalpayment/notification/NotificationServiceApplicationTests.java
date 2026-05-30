6bcfe1ec1916bf5add165fdfc24ad07b99f3e66e
khubebanjare <info.khube@gmail.com>
2026-05-30 21:30:55 +0530

    feat: add initial configuration files and application structure for digital banking platform

---
+++ notification-service/src/test/java/com/digitalpayment/notification/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalpayment.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
